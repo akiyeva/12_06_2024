d858b5bf4a5f0821542418b14284bcf2cbe5fbc3
thestarrynight17 <thestarrynight17@users.noreply.github.com>
2024-06-13 17:25:16 +0400

    AddStudents() and GetAllStudents() added

--- 12_06_2024/Group.cs
+++ 12_06_2024/Group.cs
@@ -8,39 +8,68 @@ namespace _12_06_2024
 {
     public class Group
     {
-        public int GroupNo { get; set; }
-        public int StudentLimit { get; set; }
-        public Student[] Students { get; set; }
+        private int _studentLimit;
+        public string GroupNo { get; set; }
+        public int StudentLimit
+        {
+            get => _studentLimit;
+
+            set
+            {
+                if (value > 5 && value < 18)
+                {
+                    _studentLimit = value;
+                }
+            }
+        }
+        public Student[] Students;
+        public Group(string GroupNo, int StudentLimit)
+        {
+            Students = new Student[0];
+        }
         public bool CheckGroupNo(string groupNo)
         {
+
+            int numberOfLetters = 0;
+            int numberOfDigit = 0;
+
             if (groupNo.Length != 5)
             {
                 return false;
             }
 
-
             for (int i = 0; i < 2; i++)
             {
                 if (char.IsLetter(groupNo[i]))
                 {
-
-                    for (int j = 2; j <= 5; j++)
-                    {
-                        if (char.IsDigit(groupNo[j]))
-                        {
-                            return true;
-                        }
-                        else
-                            return false;
-                    }
+                    numberOfLetters++;
                 }
-                else
-                    return false;
             }
 
+            for (int i = 2; i <= 5; i++)
+            {
+                if (char.IsDigit(groupNo[i]))
+                {
+                    numberOfDigit++;
+                }
+            }
 
+            return numberOfLetters == 2 && numberOfDigit == 3;
+        }
+
+        public void AddStudent(Student student)
+        {
+            while (Students.Length < StudentLimit)
+            {
+                Array.Resize(ref Students, Students.Length + 1);
+                Students[Students.Length + 1] = student;
+            }
+        }
+        public Student[] GetAllStudents()
+        {
+            return Students;
         }
     }
+
+
 }
-
-
